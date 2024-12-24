2123b05633f121b356a2f63879f758f605089453
shaik iliaz <shaik.iliaz1993@gmail.com>
2024-12-24 13:31:47 +0000

    checking with source and models

--- models/customer.sql
+++ models/customer.sql
@@ -1,1 +1,10 @@
-select * from {{ source('tpch_sf1', 'customer') }}
+{{
+    config(
+        materialized='table'
+    )
+}}
+
+with cte as 
+(select * from {{ source('tpch_sf1', 'customer') }})
+select * from cte 
+
